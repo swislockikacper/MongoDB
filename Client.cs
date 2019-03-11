d4d0626e25d63759d1e9dacb94ce9cd698ebbdfe
swislockikacper <k.swislocki96@gmail.com>
2019-03-11 19:36:06 +0100

    added part of crud operations

---
+++ Client.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace MongoDB
+{
+    public class Client
+    {
+        public ObjectId Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public short Age { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 
 namespace MongoDB
 {
     class Program
     {
+        protected static IMongoClient client;
+        protected static IMongoDatabase database;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            client = new MongoClient();
+            database = client.GetDatabase("Clients");
+
+            AddClient();
+
+            Console.WriteLine("Users before changes:");
+            PrintClients();
+
+            UpdateClient();
+            Console.WriteLine("Users after changes:");
+            PrintClients();
+
+            Console.ReadKey();
+        }
+
+        private static void AddClient()
+        {
+            var data = database.GetCollection<Client>("Client");
+
+            data.InsertOne(new Client
+            {
+                FirstName = "FirstName1",
+                LastName = "LastName2",
+                Age = 20
+            });
+        }
+
+        private static void UpdateClient()
+        {
+            var data = database.GetCollection<Client>("Client");
+
+            data.FindOneAndUpdate<Client>
+                (Builders<Client>.Filter.Eq("LastName", "LastName2"),
+                Builders<Client>.Update.Set("LastName", "LastName1"));
+        }
+
+        private static void PrintClients()
+        {
+            var data = database.GetCollection<Client>("Client");
+
+            var clients = data.Find(new BsonDocument());
+
+            foreach (var client in clients.ToEnumerable())
+            {
+                Console.WriteLine($"Id: {client.Id}, FirstName: {client.FirstName}, LastName: {client.LastName}, Age: {client.Age}");
+            }
         }
     }
 }
